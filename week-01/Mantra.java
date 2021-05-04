cecef708baab1cabf45d807d42032603afb2e3f2
ViggyRavilla <72957334+ViggySmallz@users.noreply.github.com>
2021-05-04 13:19:01 -0700

    Create Mantra.java

---
+++ week-01/Mantra.java
@@ -0,0 +1,10 @@
+public class Mantra {
+  public static void mantra() {
+      System.out.println("There is one important concpt that all coders must understand: ");
+      System.out.println("The system.out.println() command");
+  }  
+  public static void main(String [] args) {
+      mantra();
+      mantra();
+  }
+}
