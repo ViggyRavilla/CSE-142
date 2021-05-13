4c8ccc8df9096ffb89f44b4acd65d3a820bb365a
ViggyRavilla <72957334+ViggySmallz@users.noreply.github.com>
2021-05-12 20:59:28 -0700

    made new file and beginning NFL Project

---
+++ random/counting.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class counting {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = s.nextInt();
+        System.out.println("Enter how many numbers you want to count until");
+        int count = s.nextInt();
+        System.out.println(num);
+        s.close();
+    for(int i=1;i<=count;i++) {
+         System.out.println(num+1);
+     }   
+    }
+}
